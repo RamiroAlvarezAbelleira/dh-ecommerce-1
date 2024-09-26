756856935106891d0d58631a2252d89aea1290a9
Ramiro Alvarez Abelleira <ramiroalvarez@Ramiros-MacBook-Air.local>
2024-09-26 17:53:36 -0300

    added navbar

---
+++ src/components/ui/NavBar/NavBar.tsx
@@ -0,0 +1,23 @@
+import Logo from "../../../assets/logo.svg"
+import Cart from "../../../assets/cart.svg"
+import styles from "./NavBar.module.css"
+
+
+const NavBar = () => {
+    return (
+        <div className={styles.navbarContainer}>
+            <div className={styles.navbarDetail}>
+                <img src={Logo} alt="Logo de ecommerce" width={50} height={50} />
+                <div>
+                    <span>DH Ecommerce</span>
+                </div>
+            </div>
+            <div className={styles.navbarCartContainer}>
+                <p className={styles.navbarTextAmount}>2</p>
+                <img src={Cart} alt="Icono de carrito" />
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
