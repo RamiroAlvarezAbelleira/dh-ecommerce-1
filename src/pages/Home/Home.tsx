1d2672d3c0797c5a2b8b78b3884cfee2ec331018
Ramiro Alvarez Abelleira <ramiroalvarez@Ramiros-MacBook-Air.local>
2024-09-30 18:26:23 -0300

    created products service and using it at home page to set the products

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,24 +2,33 @@ import { useEffect, useState } from "react"
 import Hero from "../../components/ui/Hero/Hero"
 import styles from "./Home.module.css"
 import ProductCard from "../../components/ui/ProductCard/ProductCard"
-import { getProducts } from "../../service/products"
+import { getProducts } from "../../service"
+import { Products } from "../../interface"
 
 const Home = () => {
 
-  const [products, setProducts] = useState([])
-
-  
+  const [products, setProducts] = useState<Products[]>([])
+  const [error, setError] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
 
 
   useEffect(() => {
-    getProducts()
+    getProducts().then((data) => {
+      setProducts(data)
+    }).catch(() => {
+      setError(true)
+    }).finally(() => {
+      setIsLoading(false)
+    })
   }, [])
   
   return (
     <>
       <Hero />
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something went wrong...</p>}
       <div className={styles.container}>
-        {products.map((prod: any) => {
+        {products.map((prod) => {
           return <ProductCard key={prod.tail} product={prod}/>
         })}
       </div>
