eafc35e281fd1ef12fdf999e6b8ed350ddb35edc
Ramiro Alvarez Abelleira <ramiroalvarez@Ramiros-MacBook-Air.local>
2024-10-03 12:04:45 -0300

    new json-server command added to package.json and new navigations between home and checkout

--- src/components/ui/NavBar/NavBar.tsx
+++ src/components/ui/NavBar/NavBar.tsx
@@ -4,35 +4,51 @@ import styles from "./NavBar.module.css"
 import { useState } from "react"
 import CartModal from "../CartModal/CartModal"
 import useCartContext from "../../../hooks/useCartContext"
+import { useLocation, useNavigate } from "react-router-dom"
 
 
 const NavBar = () => {
 
     const [showCartModal, setShowCartModal] = useState(false)
 
+    const { state: { cartItems } } = useCartContext()
+
+    const navigate = useNavigate()
+
+    const location = useLocation()
+
     const handleShowCartModal = () => {
         setShowCartModal(!showCartModal)
     }
 
-    const { state: { cartItems } } = useCartContext()
+    const handleNavigateToHome = () => {
+        navigate("/")
+    }
 
     return (
         <div className={styles.navbarContainer}>
-            <div className={styles.navbarDetail}>
+            <div onClick={handleNavigateToHome} className={styles.navbarDetail}>
                 <img src={Logo} alt="Logo de ecommerce" width={50} height={50} />
                 <div>
                     <span>DH Ecommerce</span>
                 </div>
             </div>
-            <div className={styles.navbarCartContainer}>
-                {cartItems.length > 0 &&
-                    <p className={styles.navbarTextAmount}>{cartItems.length}</p>
-                }
-                <img src={Cart} alt="cart icon" onClick={handleShowCartModal} />
-            </div>
             {
-                showCartModal && (<CartModal handleShowCartModal={handleShowCartModal} />)
+                location.pathname !== "/checkout" &&
+                <>
+                    <div className={styles.navbarCartContainer}>
+                        {cartItems.length > 0 &&
+                            <p className={styles.navbarTextAmount}>{cartItems.length}</p>
+                        }
+                        <img src={Cart} alt="cart icon" onClick={handleShowCartModal} />
+                    </div>
+                    {
+                        showCartModal && (<CartModal handleShowCartModal={handleShowCartModal} />)
+                    }
+                </>
+
             }
+
         </div>
     )
 }
--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,9 +1,18 @@
 import Table from "../../components/ui/Table/Table"
+import styles from "./Checkout.module.css"
 
 const Checkout = () => {
   return (
-    <div>
-      <Table/>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Checkout</h1>
+      <div className={styles.grid}>
+        <div>
+          <Table />
+        </div>
+        <div>
+          {/* Form */}
+        </div>
+      </div>
     </div>
   )
 }
