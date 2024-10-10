89c3143ce73a480b10d0de821dba9422c6224bb1
Ramiro Alvarez Abelleira <alvarezramiro1997@gmail.com>
2024-10-10 15:49:05 -0300

    updated login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,45 @@
+import { useState } from "react"
 import styles from "./Login.module.css"
+import { useNavigate } from "react-router-dom"
+import { toast, Toaster } from "sonner"
 
 const Login = () => {
+
+    const navigate = useNavigate()
+
+    const [userData, setUserData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserData({
+            ...userData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if (userData.email.trim() === "" || userData.password.trim() === "") {
+            toast.error("all fields are required")
+            return;
+        }
+
+        localStorage.setItem(
+            'userLogin',
+            JSON.stringify(userData.email)
+        )
+
+        navigate("/dashboard")
+    }
+
     return (
         <div className={styles.containerLogin}>
+            <Toaster richColors visibleToasts={1} />
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 {/* Email */}
                 <div className={styles.formControlLogin}>
                     <label htmlFor="email">Email</label>
@@ -12,8 +47,8 @@ const Login = () => {
                         type="email"
                         name="email"
                         id="email"
-                        value={''}
-                        onChange={() => { }} />
+                        value={userData.email}
+                        onChange={handleChange} />
                 </div>
                 {/* Password */}
                 <div className={styles.formControlLogin}>
@@ -22,10 +57,10 @@ const Login = () => {
                         type="password"
                         name="password"
                         id="password"
-                        value={''}
-                        onChange={() => { }} />
+                        value={userData.password}
+                        onChange={handleChange} />
                 </div>
-                <div>
+                <div className={styles.formControlLogin}>
                     <button type="submit">Login</button>
                 </div>
             </form>
