f2eb26b629380f4e77164513aa022827c4723fca
Ramiro Alvarez Abelleira <alvarezramiro1997@gmail.com>
2024-11-21 20:05:43 -0300

    dark mode working

--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "../ui/NavBar/NavBar"
+import useThemeContext from "../../hooks/useThemeContext"
+import styles from "./MainLayout.module.css"
 
 
 const MainLayout = () => {
+    const {isDarkMode} = useThemeContext()
     return (
-        <div>
+        <div className={isDarkMode ? styles.darkMode : styles.lightMode}>
             <NavBar/>
             <Outlet/>
         </div>
